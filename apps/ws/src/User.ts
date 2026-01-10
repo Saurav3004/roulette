62e1fc5cad7a65ad179173eb1fb5313e627c59e1
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2026-01-10 19:29:17 +0530

    user management

--- apps/ws/src/User.ts
+++ apps/ws/src/User.ts
@@ -1,9 +1,11 @@
 import { WebSocket } from "ws"
+import { COINS } from "./types";
 
 export class User {
     name:string;
     ws:WebSocket;
     id:number;
+    locked:number;
     balance:number;
 
     constructor(id:number,name:string,ws:WebSocket){
@@ -11,5 +13,16 @@ export class User {
         this.balance = 2500;
         this.name = name;
         this.ws = ws;
+    }
+
+    bet(amount:COINS){
+        this.balance -= amount
+        this.locked += amount
+        this.ws.send(JSON.stringify({
+            type:"bet",
+            amount:amount,
+            locked:this.locked,
+            balance:this.balance
+        })) 
     }
 }
--- apps/ws/src/types.ts
+++ apps/ws/src/types.ts
@@ -2,6 +2,13 @@ export type OutgoingMessages = {
     type: "bet"
 }
 
-export type COINS = 1 | 5 | 10 | 25 | 50 | 100 | 250 | 500 | 1000
+export enum COINS {
+    One,
+    Five,
+    Ten,
+    Fifty,
+    OneHundred,
+    FiveHundred
+}
 
 export type GameState = "people-can-bet" | "people-cant-bet" | "game-over"
